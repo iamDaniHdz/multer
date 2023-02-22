18fa18041b12c0bc297a824896d3ab51da7b9671
Daniel Hernandez <daniel244@live.com.mx>
2023-02-21 23:28:59 -0600

    Terminado

--- app.js
+++ app.js
@@ -14,8 +14,30 @@ const storage = multer.diskStorage({
 
 //inicializamos upload
 const upload = multer({
-    storage: storage
+    storage: storage,
+    limits: { fileSize: 10000000 },
+    fileFilter: function (req, file, cb) {
+        checkFileType(file, cb)
+    }
 }).single('myImage')
+
+//checamos el tipo de archivo
+function checkFileType(file, cb) {
+    //lista de archivos permitidos
+    const filetypes = /jpeg|jpg|png|gif/
+
+    //verificar la extension del archivo
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+
+    //verificamos el mimetype
+    const mimetype = filetypes.test(file.mimetype)
+
+    if (mimetype && extname) {
+        cb(null, true)
+    } else {
+        cb('Error: Solo se permiten subir imágenes')
+    }
+}
 
 //inicializamos la app
 const app = express()
@@ -27,6 +49,30 @@ app.set('view engine', 'ejs')
 app.use(express.static('./public'))
 
 app.get('/', (req, res) => res.render('index'))
+
+app.post('/upload', (req, res) => {
+    //res.send('test')
+    upload(req, res, (err) => {
+        if (err) {
+            res.render('index', {
+                msg: err
+            })
+        } else {
+            //console.log(req.file)
+            //res.send('test')
+            if (req.file == undefined) {
+                res.render('index', {
+                    msg: "Error: No seleccionaste un archivo"
+                })
+            } else {
+                res.render('index', {
+                    msg: "Archivo subido correctamente",
+                    file: `uploads/${req.file.filename}`
+                })
+            }
+        }
+    })
+})
 
 const port = 3000
 
